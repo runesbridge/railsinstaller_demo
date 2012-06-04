22c479240cb226512b7c62abf0e2abd847126cdf
Delphi_Mobile <delphimobile@mail.com>
2012-06-03 20:51:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '633351046d6fb5c4c653cd268e8fe3a6d573447e1ffbc12beaaac5964e2eeb5a3630a21d54700d01f96138f2de80f2a3abf38e4609fda8295e356219fb714bfb'
